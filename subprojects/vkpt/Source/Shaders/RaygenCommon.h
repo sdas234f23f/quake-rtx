4fabd553e60bbf6b945fb81523f5a7be839c0e24
f1am3d <flamesoff@gmail.com>
2026-08-17 12:31:31 +0200

    Vendor the vkpt renderer + CMake build (from the vkquake-rt work)
    - subprojects/vkpt: vendored Q2RTX-style renderer (built via CMake subproject,
      linked as a static library into vkquake)
    - CMakeLists.txt: full CMake build ported from Meson (native Vulkan shader
      pipeline glslang/spirv-opt/bintoc + mkpak/embedded_pak, SDL3, codecs, DLL
      copies); includes the quake-rtx-specific shaders md5_debug.vert and the
      8-weight skinning variants (md5_8.vert, skinning_8.comp)
    - build_win.ps1: CMake+Ninja build script (VS Build Tools)
    - meson.build: C++ language + cmake.subproject('vkpt'), vulkan version
      relaxed to >=1.4.321 locally (only SDK 1.4.321.1 installed)
    - .gitignore: vkpt/Build/, build dirs
    
    Verified: Debug CMake build succeeds, native renderer smoke test passes.

---
+++ subprojects/vkpt/Source/Shaders/RaygenCommon.h
@@ -0,0 +1,362 @@
+// Copyright (c) 2021 Sultim Tsyrendashiev
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef RAYGEN_COMMON_H_
+#define RAYGEN_COMMON_H_
+
+#extension GL_EXT_ray_tracing : require
+
+
+
+#include "ShaderCommonGLSLFunc.h"
+
+
+
+#if !defined(DESC_SET_TLAS) || \
+    !defined(DESC_SET_GLOBAL_UNIFORM) || \
+    !defined(DESC_SET_VERTEX_DATA) || \
+    !defined(DESC_SET_TEXTURES) || \
+    !defined(DESC_SET_RANDOM) || \
+    !defined(DESC_SET_LIGHT_SOURCES)
+        #error Descriptor set indices must be set!
+#endif
+
+#define LIGHT_SAMPLE_METHOD_NONE 0
+#define LIGHT_SAMPLE_METHOD_DIRECT 1
+#define LIGHT_SAMPLE_METHOD_INDIR 2
+#define LIGHT_SAMPLE_METHOD_GRADIENTS 3
+#define LIGHT_SAMPLE_METHOD_INITIAL 4
+#define LIGHT_SAMPLE_METHOD_VOLUME 5
+#if !defined(LIGHT_SAMPLE_METHOD)
+    #error Light sampling method must be defined
+#endif
+
+
+
+#include "Surface.inl"
+#include "Light.h"
+#include "Media.h"
+#include "RayCone.h"
+
+#define HITINFO_INL_PRIM
+    #include "HitInfo.inl"
+#undef HITINFO_INL_PRIM
+
+#define HITINFO_INL_RFL
+    #include "HitInfo.inl"
+#undef HITINFO_INL_RFL
+
+#define HITINFO_INL_INDIR
+    #include "HitInfo.inl"
+#undef HITINFO_INL_INDIR
+
+
+
+layout(set = DESC_SET_TLAS, binding = BINDING_ACCELERATION_STRUCTURE_MAIN)   uniform accelerationStructureEXT topLevelAS;
+
+#ifdef DESC_SET_CUBEMAPS
+layout(set = DESC_SET_CUBEMAPS, binding = BINDING_CUBEMAPS) uniform samplerCube globalCubemaps[];
+#endif
+
+#ifdef DESC_SET_RENDER_CUBEMAP
+layout(set = DESC_SET_RENDER_CUBEMAP, binding = BINDING_RENDER_CUBEMAP) uniform samplerCube renderCubemap;
+#endif
+
+#ifdef DESC_SET_PORTALS
+layout(set = DESC_SET_PORTALS, binding = BINDING_PORTAL_INSTANCES) uniform readonly PortalInstances_BT
+{
+    ShPortalInstance g_portals[PORTAL_MAX_COUNT];
+};
+#endif
+
+
+layout(location = PAYLOAD_INDEX_DEFAULT) rayPayloadEXT ShPayload g_payload;
+
+#if LIGHT_SAMPLE_METHOD != LIGHT_SAMPLE_METHOD_NONE
+layout(location = PAYLOAD_INDEX_SHADOW) rayPayloadEXT ShPayloadShadow g_payloadShadow;
+#endif
+
+
+
+uint getPrimaryVisibilityCullMask()
+{
+    return globalUniform.rayCullMaskWorld | INSTANCE_MASK_REFRACT | INSTANCE_MASK_FIRST_PERSON;
+}
+
+uint getReflectionRefractionCullMask(uint surfInstCustomIndex, uint geometryInstanceFlags, bool isRefraction)
+{
+    uint world = globalUniform.rayCullMaskWorld | INSTANCE_MASK_REFRACT;
+
+    if( ( geometryInstanceFlags & GEOM_INST_FLAG_IGNORE_REFRACT_AFTER ) != 0 )
+    {
+        // ignore refract geometry if requested
+        world = world & ( ~INSTANCE_MASK_REFRACT );
+
+        // if it's also a first-person geometry, then ignore everything first-person
+        if ((surfInstCustomIndex & INSTANCE_CUSTOM_INDEX_FLAG_FIRST_PERSON) != 0)
+        {
+            return world;
+        }
+    }
+
+    if ((surfInstCustomIndex & INSTANCE_CUSTOM_INDEX_FLAG_FIRST_PERSON) != 0)
+    {
+        // ignore first-person viewer -- on first-person
+        return world | INSTANCE_MASK_FIRST_PERSON;
+    }
+    
+    return isRefraction ? 
+        // no first-person viewer in refractions
+        world | INSTANCE_MASK_FIRST_PERSON :
+        // no first-person in reflections
+        world | INSTANCE_MASK_FIRST_PERSON_VIEWER;
+}
+
+uint getShadowCullMask(uint surfInstCustomIndex)
+{
+    const uint world = globalUniform.rayCullMaskWorld_Shadow;
+
+    if ((surfInstCustomIndex & INSTANCE_CUSTOM_INDEX_FLAG_FIRST_PERSON) != 0)
+    {
+        // no first-person viewer shadows -- on first-person
+        return world | INSTANCE_MASK_FIRST_PERSON;
+    }
+    else if ((surfInstCustomIndex & INSTANCE_CUSTOM_INDEX_FLAG_FIRST_PERSON_VIEWER) != 0)
+    {
+        // no first-person shadows -- on first-person viewer
+        return world | INSTANCE_MASK_FIRST_PERSON_VIEWER;
+    }
+    else
+    {
+        // no first-person shadows -- on world
+        return world | INSTANCE_MASK_FIRST_PERSON_VIEWER;
+    }
+}
+
+uint getIndirectIlluminationCullMask(uint surfInstCustomIndex)
+{
+    const uint world = globalUniform.rayCullMaskWorld;
+    
+    if ((surfInstCustomIndex & INSTANCE_CUSTOM_INDEX_FLAG_FIRST_PERSON) != 0)
+    {
+        // no first-person viewer indirect illumination -- on first-person
+        return world | INSTANCE_MASK_FIRST_PERSON;
+    }
+    else if ((surfInstCustomIndex & INSTANCE_CUSTOM_INDEX_FLAG_FIRST_PERSON_VIEWER) != 0)
+    {
+        // no first-person indirect illumination -- on first-person viewer
+        return world | INSTANCE_MASK_FIRST_PERSON_VIEWER;
+    }
+    else
+    {
+        // no first-person indirect illumination -- on first-person viewer
+        return world | INSTANCE_MASK_FIRST_PERSON_VIEWER;
+    }
+}
+
+
+
+uint getAdditionalRayFlags()
+{
+    return globalUniform.rayCullBackFaces != 0 ? gl_RayFlagsCullFrontFacingTrianglesEXT : 0;
+}
+
+
+
+bool doesPayloadContainHitInfo(const ShPayload p)
+{
+    if (p.instIdAndIndex == UINT32_MAX || p.geomAndPrimIndex == UINT32_MAX)
+    {
+        return false;
+    }
+
+    int instanceId, instanceCustomIndex;
+    unpackInstanceIdAndCustomIndex(p.instIdAndIndex, instanceId, instanceCustomIndex);
+
+    if ((instanceCustomIndex & INSTANCE_CUSTOM_INDEX_FLAG_SKY) != 0)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void resetPayload()
+{
+    g_payload.baryCoords = vec2(0.0);
+    g_payload.instIdAndIndex = UINT32_MAX;
+    g_payload.geomAndPrimIndex = UINT32_MAX;
+}
+
+ShPayload tracePrimaryRay(vec3 origin, vec3 direction)
+{
+    resetPayload();
+
+    uint cullMask = getPrimaryVisibilityCullMask();
+
+    traceRayEXT(
+        topLevelAS,
+        getAdditionalRayFlags(), 
+        cullMask, 
+        0, 0,     // sbtRecordOffset, sbtRecordStride
+        SBT_INDEX_MISS_DEFAULT, 
+        origin, globalUniform.primaryRayMinDist, direction, globalUniform.rayLength, 
+        PAYLOAD_INDEX_DEFAULT);
+
+    return g_payload; 
+}
+
+ShPayload traceReflectionRefractionRay(vec3 origin, vec3 direction, uint surfInstCustomIndex, uint geometryInstanceFlags, bool isRefraction)
+{
+    resetPayload();
+
+    uint cullMask = getReflectionRefractionCullMask(surfInstCustomIndex, geometryInstanceFlags, isRefraction);
+
+    traceRayEXT(
+        topLevelAS,
+        getAdditionalRayFlags(), 
+        cullMask, 
+        0, 0,     // sbtRecordOffset, sbtRecordStride
+        SBT_INDEX_MISS_DEFAULT, 
+        origin, 0.001, direction, globalUniform.rayLength, 
+        PAYLOAD_INDEX_DEFAULT);
+
+    return g_payload; 
+}
+
+ShPayload traceIndirectRay(uint surfInstCustomIndex, vec3 surfPosition, vec3 bounceDirection)
+{
+    resetPayload();
+
+    uint cullMask = getIndirectIlluminationCullMask(surfInstCustomIndex);
+
+    traceRayEXT(
+        topLevelAS,
+        getAdditionalRayFlags(), 
+        cullMask, 
+        0, 0,     // sbtRecordOffset, sbtRecordStride
+        SBT_INDEX_MISS_DEFAULT, 
+        surfPosition, 0.001, bounceDirection, globalUniform.rayLength, 
+        PAYLOAD_INDEX_DEFAULT); 
+
+    return g_payload;
+}
+
+
+
+#ifdef DESC_SET_CUBEMAPS
+// Get sky color for primary visibility, i.e. without skyColorMultiplier
+vec3 getSkyPrimary(vec3 direction)
+{
+    uint skyType = globalUniform.skyType;
+
+#ifdef DESC_SET_RENDER_CUBEMAP
+    if (skyType == SKY_TYPE_RASTERIZED_GEOMETRY || skyType == SKY_TYPE_PROCEDURAL)
+    {
+        return texture(renderCubemap, direction).rgb;
+    }
+#endif
+
+    if (skyType == SKY_TYPE_CUBEMAP)
+    {
+        direction = mat3(globalUniform.skyCubemapRotationTransform) * direction;
+        
+        return texture(globalCubemaps[nonuniformEXT(globalUniform.skyCubemapIndex)], direction).rgb;
+    }
+
+    return globalUniform.skyColorDefault.xyz;
+}
+
+vec3 getSky(vec3 direction)
+{
+    vec3 col = getSkyPrimary(direction);
+    return col * globalUniform.skyColorMultiplier;
+}
+#endif
+
+
+
+#if LIGHT_SAMPLE_METHOD != LIGHT_SAMPLE_METHOD_NONE
+
+#define SHADOW_RAY_EPS       0.01
+#define RAY_ORIGIN_LEAK_BIAS 0.01    // offset a bit towards a viewer to prevent light leaks from the other side of polygons
+
+bool traceShadowRay(uint surfInstCustomIndex, vec3 start, vec3 end, bool ignoreFirstPersonViewer /* = false */)
+{
+    // prepare shadow payload
+    g_payloadShadow.isShadowed = 1;  
+
+    uint cullMask = getShadowCullMask(surfInstCustomIndex);
+
+    if (ignoreFirstPersonViewer)
+    {
+        cullMask &= ~INSTANCE_MASK_FIRST_PERSON_VIEWER;
+    }
+
+    vec3 l = end - start;
+    float maxDistance = length(l);
+    l /= maxDistance;
+
+    traceRayEXT(
+        topLevelAS, 
+        gl_RayFlagsSkipClosestHitShaderEXT | getAdditionalRayFlags(), 
+        cullMask, 
+        0, 0, 	// sbtRecordOffset, sbtRecordStride
+        SBT_INDEX_MISS_SHADOW, 		// shadow missIndex
+        start, 0.001, l, maxDistance - SHADOW_RAY_EPS, 
+        PAYLOAD_INDEX_SHADOW);
+
+    return g_payloadShadow.isShadowed == 1;
+}
+
+float traceVisibility(const Surface surf, const vec3 lightPosition, uint lightIndex)
+{
+    const vec3 start = surf.position + surf.toViewerDir * RAY_ORIGIN_LEAK_BIAS;
+    const vec3 end = lightPosition;
+
+    const bool ignoreFirstPersonViewer = (globalUniform.lightIndexIgnoreFPVShadows == lightIndex);
+
+    const bool isShadowed = traceShadowRay(surf.instCustomIndex, start, end, ignoreFirstPersonViewer);
+    return float(!isShadowed);
+}
+#endif // LIGHT_SAMPLE_METHOD != LIGHT_SAMPLE_METHOD_NONE
+
+
+
+void shade(const Surface surf, const LightSample light, float oneOverPdf, out vec3 diffuse, out vec3 specular)
+{
+    vec3 l = safeNormalize(light.position - surf.position);
+    float nl = dot(surf.normal, l);
+    float ngl = dot(surf.normalGeom, l);
+
+    if (nl <= 0 || ngl <= 0)
+    {
+        diffuse = specular = vec3(0);
+        return;
+    }
+
+    diffuse  = light.dw * nl * light.color * evalBRDFLambertian(1.0);
+    specular = light.dw * nl * light.color * evalBRDFSmithGGX(surf.normal, surf.toViewerDir, l, surf.roughness, surf.specularColor);
+
+    diffuse  *= oneOverPdf;
+    specular *= oneOverPdf;
+}
+
+#endif // RAYGEN_COMMON_H_
